397c0984cf26df46c91980ab58566c2a8cf82b03
adrdur26-collab <adrdur26@bergen.org>
2025-10-31 10:26:06 -0400

    basics

--- Lesson 1 - Introduction/basics.py
+++ Lesson 1 - Introduction/basics.py
@@ -22,29 +22,35 @@
 
 # draw_square()
 
-def morning_routine():
-    print("Morning Routine")
-    print("Brush Teeth")
-    print("Shower")
-    print("Do Hair")
+# def morning_routine():
+#     print("Morning Routine")
+#     print("Brush Teeth")
+#     print("Shower")
+#     print("Do Hair")
 
-def school_schedule():
-    print("School Schedule")
-    print("Period 1 - Calculus")
-    print("Period 2 - Health")
-    print("Period 3 - Biology")
+# def school_schedule():
+#     print("School Schedule")
+#     print("Period 1 - Calculus")
+#     print("Period 2 - Health")
+#     print("Period 3 - Biology")
 
-def after_school():
-    print("After School Routine")
-    print("Go to Gym")
-    print("Do Homework")
-    print("Eat Dinner")
+# def after_school():
+#     print("After School Routine")
+#     print("Go to Gym")
+#     print("Do Homework")
+#     print("Eat Dinner")
 
-def daily_routine():
-    morning_routine()
-    print()
-    school_schedule()
-    print()
-    after_school()
+# def daily_routine():
+#     morning_routine()
+#     print()
+#     school_schedule()
+#     print()
+#     after_school()
 
-daily_routine()
+# daily_routine()
+
+def calc_area_rect(w,l):
+    area = w * l
+    # print(area)
+    print(f"Area of {w}{l}{area}")
+calc_area_rect(5,10)
