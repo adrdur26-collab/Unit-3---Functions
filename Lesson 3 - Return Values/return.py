963eb1377e8672c6dcde6a80164fc954ee9016be
adrdur26-collab <adrdur26@bergen.org>
2025-11-04 11:55:44 -0500

    return

---
+++ Lesson 3 - Return Values/return.py
@@ -0,0 +1,95 @@
+"""
+Lesson 3: Return Values - Practice Exercises
+"""
+# def get_letter_grade(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     else:
+#         return "F"
+# result = get_letter_grade(80)
+# print(get_letter_grade(80))
+
+
+# def check_age(age):
+#     if age >= 18:
+#         return "Adult"
+#         print("This never runs")
+#     return "Minor"
+
+
+# def square(num):
+#     return num ** 2
+# result = square(5)
+# print(f"5 sqaured is {result}")
+# area = square(7) * 2
+# print(f"Area: {area}")
+
+
+# def greet(name):
+#     # print(f"Hello {name}")
+#     return f"Hello {name}"
+
+
+# result = greet("Adriel")
+# print(result)
+
+# ============================================================================
+# Practice 1: Rectangle Area
+# ============================================================================
+
+# def rect_area(w, l):
+    
+#     return w * l
+
+# rect1 = rect_area(3, 5)
+# print(f"rectangle 1's area: {rect1}")
+# rect2 = rect_area(8, 9)
+# print(f"rectangle 1's area: {rect2}")
+
+# ============================================================================
+# Practice 2: Temperature Converter
+# ============================================================================
+
+def celsius_to_farenheit(celsius): #(C*9/5) + 32
+    'convert celsius to Farenheit'
+    return (celsius * 9/5) + 32
+print(f"")
+# ============================================================================
+# Practice 3: Find Maximum
+# ============================================================================
+
+def find_max(num1, num2):
+    if num1 > num2:
+        return num1
+    return num2 #Alternative return max(num1, num2)
+
+# ============================================================================
+# Practice 4: Is Even?
+# ============================================================================
+
+
+
+
+
+# ============================================================================
+# Practice 5: Calculate Tip
+# ============================================================================
+
+
+
+
+# ============================================================================
+# Challenge 1: Calculate Average
+# ============================================================================
+
+
+
+
+
+# ============================================================================
+# Challenge 2: Letter Grade
+# ============================================================================
