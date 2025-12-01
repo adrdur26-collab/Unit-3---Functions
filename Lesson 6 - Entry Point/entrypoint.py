49b763e48f45f966db5eae7d1ce71bf9c459c5e8
adrdur26-collab <adrdur26@bergen.org>
2025-12-01 10:34:22 -0500

    entpointgame

---
+++ Lesson 6 - Entry Point/entrypoint.py
@@ -0,0 +1,8 @@
+def square(n):
+    return n * n
+
+print("Testing1:", square(2))
+
+if __name__ == "__main__": #if run directly
+    # print(f"__name__ is: {__name__}")
+    print("Testing2:", square(5))
---
+++ Lesson 6 - Entry Point/game.py
@@ -0,0 +1,3 @@
+from entrypoint import square
+# print(f"__name__ is: {__name__}")
+print(f"Damage: {square(10)}")
